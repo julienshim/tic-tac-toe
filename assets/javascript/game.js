e1a33037388bac311a70fd618f03fdef4a8ccd8f
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-02-05 21:28:38 -0800

    center styling plus basic click confirmation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,14 @@
 // alert("Connected!")
+
+const boxes = document.querySelectorAll(".box");
+
+boxes.forEach(function(b){
+  b.addEventListener("click", function(event){
+    event.preventDefault();
+    if (event.srcElement.dataset.box % 2 === 0) {
+      b.innerText = "X";
+    } else {
+      b.innerText = "O";
+    }
+  })
+})
