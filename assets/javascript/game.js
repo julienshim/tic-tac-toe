bc5a21d0a02fd42046b2ae31457a0f01244fd6d4
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-02-09 19:18:10 -0800

    reset button, initial winning positions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,33 @@
 // alert("Connected!")
+let oBoard;
+
+const winningPositions = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7]
+]
 
 const boxes = document.querySelectorAll(".box");
+const restartButton = document.getElementById("restartButton");
 
-boxes.forEach(function(b){
-  b.addEventListener("click", function(event){
-    event.preventDefault();
-    if (event.srcElement.dataset.box % 2 === 0) {
-      b.innerText = "X";
-    } else {
-      b.innerText = "O";
-    }
+restartButton.addEventListener("click", startGame, false);
+
+
+
+startGame();
+
+function startGame() {
+  oBoard = Array.from(Array(9).keys(), x => x + 1);
+  boxes.forEach(function(b){
+    b.innerText = "";
+    b.addEventListener("click", function(event){
+      event.preventDefault();
+      b.innerText = event.srcElement.dataset.box;
+    }, false)
   })
-})
+};
